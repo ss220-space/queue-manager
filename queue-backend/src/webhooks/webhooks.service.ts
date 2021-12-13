79a774d9b0ac0ab2a8b76f7921683a4dbdf90c4d
Bizzonium <azizonkg@gmail.com>
2021-12-13 10:23:36 +0300

    Feat: new Webhooks module

---
+++ queue-backend/src/webhooks/webhooks.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { RedisService } from 'nestjs-redis';
+import { StatusDto } from './dto/status.dto';
+
+@Injectable()
+export class WebhooksService {
+  private readonly redis: any;
+  constructor(
+    private readonly redisService: RedisService,
+  ) { 
+    this.redis = this.redisService.getClient()
+  }
+
+  async pushStatus(server_port: string, body: StatusDto): Promise<boolean> {
+    await this.redis.set(`byond_status_${server_port}`, JSON.stringify(body))
+    return true
+  }
+}
