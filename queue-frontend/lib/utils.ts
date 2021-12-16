9cc466155efeae2585ae97c2c33ebcccd5163fa3
Bizzonium <azizonkg@gmail.com>
2021-12-16 03:44:38 +0300

    Feat: cleanup frontend dir

--- queue-frontend/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import queueConfig from '@/queue.config.json'
-
-export function assert(condition: any, message: string): asserts condition {
-  if (!condition) throw new Error(message)
-}
-
-export function checkIfServerValid(input: string | number): boolean {
-  return (input in queueConfig.servers)
-}
