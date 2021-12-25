bfcc5c2555c6f566b9393dd69d453bca089a7a19
Bizzonium <azizonkg@gmail.com>
2021-12-25 23:24:56 +0300

    Feat(backend): add usage example of RBAC

--- queue-backend/src/byond/byond.controller.ts
+++ queue-backend/src/byond/byond.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, HttpStatus, HttpException, Get, Param } from '@nestjs/common'
+import { Controller, HttpStatus, HttpException, Get, Param, UseGuards } from '@nestjs/common'
 import { ByondService } from './byond.service'
 import { ServerPortDto } from '../common/dto/serverPort.dto'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
+import { Roles } from '../roles/roles.decorator'
+import { AdminFlag } from '../roles/adminFlag.enum'
+import { RolesGuard } from '../roles/roles.guard'
 
-@Controller('/api/byond')
+@Controller('byond')
 export class ByondController {
   constructor(private readonly byondService: ByondService) { }
 
   @Get('playerlist_ext/:server_port')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(AdminFlag.R_ADMIN, AdminFlag.R_BAN)
   async getPlayerlistExt(@Param() { server_port }: ServerPortDto): Promise<string> {
     const result = await this.byondService.getPlayerlistExt(server_port)
     if (!result) {
