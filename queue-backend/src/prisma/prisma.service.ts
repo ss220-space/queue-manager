eac02a3c91c873c88f69e0f4ff0d161a0e588537
Bizzonium <azizonkg@gmail.com>
2022-01-02 17:20:38 +0300

    Feat(backend): enable Prisma DB requests logging

--- queue-backend/src/prisma/prisma.service.ts
+++ queue-backend/src/prisma/prisma.service.ts
@@ -2,8 +2,12 @@ import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient
-  implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'warn', 'error', 'info'],
+    })
+  }
 
   async onModuleInit(): Promise<void> {
     await this.$connect();
