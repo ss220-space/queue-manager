8117836098b0828eff9d53ec6662888f2931fa50
Bizzonium <azizonkg@gmail.com>
2022-01-04 12:19:43 +0300

    Feat(frontend): add branding

--- queue-frontend/src/CommonNavBar/CommonNavBar.tsx
+++ queue-frontend/src/CommonNavBar/CommonNavBar.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import { Container, Nav, Navbar } from 'react-bootstrap'
-
 
 export type CommonNavBarProps = {
   token: string
@@ -8,10 +8,19 @@ export type CommonNavBarProps = {
 
 export function CommonNavBar({token, isAdmin}: CommonNavBarProps) {
   return (<Navbar className="common-nav" variant="dark">
-    <Container className="m-0">
-      <Navbar.Brand href={`/#token=${token}`}>
-        SS220
-      </Navbar.Brand>
+    <Container className="mx-0">
+      <Container>
+        <Navbar.Brand href={`/#token=${token}`} className="fw-bold fw">
+          <img
+            src="/ss220.logo.32.png"
+            alt="SS220 Logo"
+            width={30}
+            height={30}
+            className="d-inline-block align-top"
+          />{' '}
+          SS220
+        </Navbar.Brand>
+      </Container>
       <Nav className="me-auto">
         {
           isAdmin && <Nav.Link href={`/admin#token=${token}`}>Админка</Nav.Link>
