58b38566776192cd905ee374a1929682709939b0
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2022-01-07 16:50:52 +0300

    Refactor(frontend): Make token optional for requestBackendData

--- queue-frontend/src/utils.ts
+++ queue-frontend/src/utils.ts
@@ -3,15 +3,20 @@ export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 
 export function requestBackendData(
   url: string,
-  token: string,
+  token?: string,
   method?: string
 ): Promise<Response> {
+  let auth = {}
+  if (token) {
+    auth = { 'Authorization': `Bearer ${token}` }
+  }
+
   return fetch(
     `${backendUrl}${url}`,
     {
       method: method,
       headers: {
-        'Authorization': `Bearer ${token}`,
+        ...auth,
         'pragma': 'no-cache',
         'cache-control': 'no-cache',
       }
