c8e72f053df378d47bba0bda00cbaf9d16bfb3e2
Bizzonium <azizonkg@gmail.com>
2022-01-10 17:18:00 +0300

    Feat(frontend): add payload arg to requestBackendData function

--- queue-frontend/src/utils.ts
+++ queue-frontend/src/utils.ts
@@ -4,12 +4,15 @@ export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 export function requestBackendData(
   url: string,
   token?: string,
-  method?: string
+  method?: string,
+  payload?: any,
 ): Promise<Response> {
   let auth = {}
   if (token) {
     auth = { 'Authorization': `Bearer ${token}` }
   }
+
+  const payloadHeader = payload ? { 'Content-Type': 'application/json' } : null
 
   return fetch(
     `${backendUrl}${url}`,
@@ -17,9 +20,11 @@ export function requestBackendData(
       method: method,
       headers: {
         ...auth,
+        ...payloadHeader,
         'pragma': 'no-cache',
         'cache-control': 'no-cache',
-      }
+      },
+      body: payload ? JSON.stringify(payload) : null
     }
   )
 }
